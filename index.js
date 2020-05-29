b0e002370b5ace9675a76ade39123b09264be1b7
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-29 10:51:57 +0100

    Template created for HBS

---
+++ index.js
@@ -0,0 +1,30 @@
+// npm i express express-handlebars
+const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
+const app = express(); // initialising express to be used as a function
+
+require('dotenv').config(); // create a .env file and .gitignore
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: 'hbs'
+}))
+
+app.set('view engine', '.hbs');
+//view that I'm rendering plus the engine ext name
+
+app.get('/', async (req, res) => {
+
+    res.render('index');
+})
+
+app.get('/harry', async (req, res) => {
+    res.render('harry');
+})
+
+app.listen(3005, () => {
+    console.log('Server is running. Chase it.');
+})
